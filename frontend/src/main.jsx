d865a8b09c6502e9598128bbf8c710b7d3aa1932
Kiri Lefebvre <61973586+Kira-Le@users.noreply.github.com>
2026-05-10 17:10:08 +1000

    Update main.jsx to link to alternate frontend in hba-stroke-classifier

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
@@ -10,4 +10,9 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+)*/
+import { createRoot } from 'react-dom/client'
+import '../hba-stroke-classifier/styles.css'
+import App from '../hba-stroke-classifier/app'
+
+createRoot(document.getElementById('root')).render(<App />)
